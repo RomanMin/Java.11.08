287cfeb18fc492dd8f21835cf82d3e5a74de4412
RomanMin <1@ali.m-rv.ru>
2021-08-21 13:54:22 +0300

    add lesson 02

--- src/lesson02/Homework2.java
+++ src/lesson02/Homework2.java
@@ -2,9 +2,14 @@ package lesson02;
 // Minaev Roman
 
 public class Homework2 {
+    private static String word;
+
     public static void main(String[] args) {
         checkSum() ;
         checkNumber();
+        checkNumberNew();
+      // printWord();
+
 
     }
     public static void checkSum (){ int a = -7;
@@ -15,7 +20,7 @@ public class Homework2 {
                     System.out.println("false");
                 else System.out.println("true");
     }
-    public static void checkNumber (){ int a = -3;
+    public static void checkNumber (){ int a = 0;
         if (a < 0)
             System.out.println("Отрицательное число");
         else
@@ -23,6 +28,24 @@ public class Homework2 {
 
 
     }
-}
+    public static boolean checkNumberNew ( ) { int a=-3 ;
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
 
-    
+
+    }
+    public static void printWord ( int count = 5, String word:"Hello" ) {
+        for ( int i=1; i <= count; i++ )
+            System.out.println("String #" + i + ":" + word );
+    }
+    }
+
+
+
+
+
+
+
